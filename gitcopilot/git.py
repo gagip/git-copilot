7a7c637b20ff1c82a75ac6d30e32635a29e4cb6c
gagip <y02645@gmail.com>
2025-05-07 13:00:51 +0900

    refactor CommitInfo dataclass 추가

--- gitcopilot/git.py
+++ gitcopilot/git.py
@@ -1,46 +1,70 @@
-from git import Repo
+from git import Commit, Repo
 import pandas as pd
+from dataclasses import dataclass
+from datetime import datetime
+
+
+@dataclass
+class CommitInfo:
+    commit_id: str
+    author: str
+    datetime: datetime
+    message: str
+
+
+def _create_commit_info(commit: Commit) -> CommitInfo:
+    commit_message = ""
+    if isinstance(commit.message, bytes):
+        commit_message = commit.message.decode("utf-8")
+    elif isinstance(commit.message, str):
+        commit_message = commit.message
+
+    return CommitInfo(
+        commit_id=commit.hexsha,
+        author=f"{commit.author.name} <{commit.author.email}>",
+        datetime=commit.committed_datetime,
+        message=commit_message,
+    )
 
 
 class Repository:
     def __init__(self, repo_path: str) -> None:
         self.repo = Repo(repo_path)
 
-    def generate_to_data_frame(self, before_branch_name: str, after_branch_name: str) -> pd.DataFrame:
-        commit_data = []
-        for commit in self.repo.iter_commits(f'{before_branch_name}..{after_branch_name}'):
-            commit_info = {
-                'commit_id': commit.hexsha,
-                'author': f'{commit.author.name} <{commit.author.email}>',
-                'datetime': commit.committed_datetime,
-                'message': commit.message
-            }
-            commit_data.append(commit_info)
-        
-        return pd.DataFrame(commit_data)
+    def generate_to_data_frame(
+        self, 
+        before_branch_name: str, 
+        after_branch_name: str
+    ) -> pd.DataFrame:
+        commit_info_list = []
+        for commit in self.repo.iter_commits(
+            f"{before_branch_name}..{after_branch_name}"
+        ):
+            commit_info = _create_commit_info(commit)
+            commit_info_list.append(commit_info)
 
-    def generate_commit_data(self, before_branch_name: str, after_branch_name: str) -> str:
-        commit_data = []
-        for commit in self.repo.iter_commits(f'{before_branch_name}..{after_branch_name}'):
-            commit_info = (
-                f"Commit ID: {commit.hexsha}\n"
-                f"Author: {commit.author.name} <{commit.author.email}>\n"
-                f"Date: {commit.committed_datetime}\n"
-                f"Message: {commit.message}"
-            )
-            commit_data.append(commit_info)
+        return pd.DataFrame(commit_info_list)
+
+    def generate_commit_data(
+        self, 
+        before_branch_name: str, 
+        after_branch_name: str
+    ) -> str:
+        commit_info_list = []
         
-        return '\n\n'.join(commit_data)  # 각 커밋 정보를 두 개의 개행 문자로 구분
-    
+        commits = self.repo.iter_commits(
+            f"{before_branch_name}..{after_branch_name}"
+        )
+        for commit in commits:
+            commit_info = _create_commit_info(commit)
+            commit_info_list.append(commit_info)
+
+        return "\n\n".join(str(commit_info) for commit_info in commit_info_list)
+
     def get_commit_info(self, branch_name: str) -> pd.DataFrame:
-        commit_data = []
+        commit_info_list = []
         for commit in self.repo.iter_commits(branch_name):
-            commit_info = {
-                'commit_id': commit.hexsha,
-                'author': f'{commit.author.name} <{commit.author.email}>',
-                'datetime': commit.committed_datetime,
-                'message': commit.message
-            }
-            commit_data.append(commit_info)
-        
-        return pd.DataFrame(commit_data)
+            commit_info = _create_commit_info(commit)
+            commit_info_list.append(commit_info)
+
+        return pd.DataFrame(commit_info_list)
